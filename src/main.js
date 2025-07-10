eeddba2158fb2b6f55285da81b23d5f5f4dd6eee
LuaFerGG <luafergg@gmail.com>
2025-07-09 19:02:30 -0500

    style: format main.js for improved readability

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import i18n from './i18n'
+import i18n from './i18n'  // si lo estás usando
 
-createApp(App).use(i18n).mount('#app')
+createApp(App)
+    .use(i18n)
+    .mount('#app')
